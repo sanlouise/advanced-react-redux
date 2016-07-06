977b89565ac23674b3aa0635d4590a552777aecb
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-06 19:33:28 +0200

    Add basic test for comment_list Component and create basic component

---
+++ src/components/comment_list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//Functional component
+const CommentList = (props) => {
+	return (
+		<ul className="comment-list">
+
+		</ul>
+	);
+};
+
+export default CommentList;
+
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -8,8 +8,12 @@ describe('Test the app component', () => {
 	// Use 'it' to test single attribute of target
 	it('Displays the comment box', () => {
 		// Use 'expect' to assert something about a target
-		expect(component.find('.comment-box'));
+		expect(component.find('.comment-box')).to.exist;
 
+	});
+
+	it('Shows the comment list', () => {
+		expect(component.find('.comment-list')).to.exist;
 	});
 
 });
