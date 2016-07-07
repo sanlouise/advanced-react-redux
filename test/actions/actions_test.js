0f5dbf368ef24d7bde3cf3cb6ad0f78df9ab3c5c
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-07 20:27:07 +0200

    Add basic saveComments action creator and tests

---
+++ test/actions/actions_test.js
@@ -0,0 +1,16 @@
+import { expect } from '../test/helper';
+import { SAVE_COMMENT } from '../../src/actions/types';
+import { saveComment } from '../../src/actions';
+
+
+describe('actions', () => {
+	describe('saveComment', () => {
+		it('Has the right type', () => {
+
+		})
+
+		it('has the right payload', () => {
+
+		}
+	})
+})
