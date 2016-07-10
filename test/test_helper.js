7d4b280a9e70df82f2ebff5b5c76e0ed0bdeced5
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-10 19:25:17 +0200

    Set up jsdom for browser-like terminal use

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,4 +1,7 @@
-// Set up tetsing environment to run like browser in command line
+import jsdom from 'jsdom';
+// Set up testing environment to run like browser in command line
+global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
+global.window = global.document.defaultView;
 
 // Build 'renderComponent' helper to render react class
 
