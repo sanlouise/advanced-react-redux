355c956d5657faa4c5df151b897ed370162752d0
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-07-14 22:14:36 +0200

    Set up jQuery in test_helper

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,13 @@
 import jsdom from 'jsdom';
+import jquery from 'jquery';
+
 // Set up testing environment to run like browser in command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
+
+//Because we imported jQuery with custom 'jquery' var name. It tells jQuery to not 
+//reach out and find the DOM, but to just be responsible for the fake DOM we just created.
+const $ = jquery(global.window);
 
 // Build 'renderComponent' helper to render react class
 
